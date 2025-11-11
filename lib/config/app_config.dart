9349d65462bf7a2f54ab53d08fe7908bb2c133dc
Gabriel Resende Spirlandelli <grspirlandelli@gmail.com>
2025-11-11 18:21:53 -0300

    Pubspec.yaml

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   // LÊ A VARIÁVEL DE AMBIENTE DIRETAMENTE DO ARQUIVO .ENV
-  static String get baseUrl => dotenv.env['DIVIDE_AI_BASE_URL'] ?? 'http://localhost:8080';
+  static String get baseUrl => dotenv.env['DIVIDE_AI_BASE_URL'] ?? 'https://divide-ai-api-i8en.onrender.com';
 }
